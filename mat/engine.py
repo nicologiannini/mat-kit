f4133189d754f34f0e4a7845aab7c959f5ea5e5c
Nicolò Giannini <contact2nezz@yahoo.it>
2022-04-21 18:22:28 +0200

    Added determinant calculation

--- mat/engine.py
+++ mat/engine.py
@@ -1,6 +1,8 @@
 ##
 # Matrix Engine 0.0.1
 
+from fractions import Fraction
+
 ERRORS = {
     0: "generic internal error",
     1: "conversion not possible",
@@ -8,22 +10,23 @@ ERRORS = {
     3: "multiplication not applicable"
 }
 
-##
-# Helper
-
 def _to_column(data):
     return [[data[j][i] for j in range(0, len(data))] for i in range(0, len(data[0]))]
 
+def _is_scalar(value):
+    return isinstance(value, int) or isinstance(value, float) or isinstance(value, Fraction)
+
 def _sum(row_a, row_b):
     return [row_a[i] + row_b[i] for i in range(0, len(row_a))]
+
+def _sub(row_a, row_b):
+    return [row_a[i] - row_b[i] for i in range(0, len(row_a))]
 
 def _by_scalar(row, s):
     return [n * s for n in row]
 
 def _by_columns(row, columns):
     return [sum([row[i] * col[i] for i in range(0, len(row))]) for col in columns]
-
-# Base class
 
 class Matrix:
     "stores a two-dimensional array"
@@ -45,7 +48,7 @@ class Matrix:
 
     def __mul__(self, other):
         out = []
-        if isinstance(other, int) or isinstance(other, float):
+        if _is_scalar(other):
             out = [_by_scalar(row, other) for row in self.rows]
         else:
             other = other if isinstance(other, Matrix) else Matrix(other)
@@ -69,8 +72,23 @@ class Matrix:
     def transpose(self):
         return Matrix(self.columns)
 
-    def get_determinant(self):  # TODO
-        pass
+    def get_determinant(self):
+        assert self.is_squared(), ERRORS.get(0)
+        ut_mat = self.to_upper_triagular()
+        det = 1
+        for i in range(0, self.n_rows):
+            det *= ut_mat.rows[i][i]
+        return det
 
-    def get_inverse(self):  # TODO
+    def to_upper_triagular(self):
+        mat_copy = Matrix(self.rows)
+        for i in range(0, self.n_rows):
+            for j in range(0, i):
+                if mat_copy.rows[i][j] != 0:
+                    t = Fraction(mat_copy.rows[i][j], mat_copy.rows[j][j])
+                    temp_row = _by_scalar(mat_copy.rows[j], t)
+                    mat_copy.rows[i] = _sub(mat_copy.rows[i], temp_row)
+        return mat_copy
+
+    def get_inverse(self):
         pass
--- mat/test.py
+++ mat/test.py
@@ -1,4 +1,5 @@
 from engine import Matrix
+from fractions import Fraction
 
 a = [[3, -1, 2], [2, 0, 1]]
 b = [[2, -3], [1, 2], [0, 4]]
@@ -7,3 +8,10 @@ a = Matrix(a)
 b = Matrix(b)
 c = a * b
 print(c)
+
+print(Fraction(2, 5) + 2)
+
+c = [[1, 2, 3], [4, 5, 4], [3, 2 , 1]]
+c = Matrix(c)
+print(c.to_upper_triagular())
+print(c.get_determinant())
