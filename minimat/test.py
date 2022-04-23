5dbc018c2fe03c52dddc911c5ee1121aec2bea60
Nicolò Giannini <contact2nezz@yahoo.it>
2022-04-23 21:54:24 +0200

    inverse matrix test added

--- minimat/test.py
+++ minimat/test.py
@@ -6,6 +6,8 @@ AB = [[5, -3], [4, -2]]
 A2 = [[6, -2, 4], [4, 0, 2]]
 C = [[1, 2, 3], [4, 5, 4], [3, 2, 1]]
 CT = [[1, 4, 3], [2, 5, 2], [3, 4, 1]]
+D = [[1, 2], [2, 3]]
+DR = [[-3, 2], [2, -1]]
 
 def test_operations():
     a = Matrix(A)
@@ -17,5 +19,7 @@ def test_operations():
 def test_transformation():
     c = Matrix(C)
     c.transpose()
+    d = Matrix(D)
     assert c.get_determinant() == -8
     assert c.rows == CT
+    assert d.get_inverse().rows == DR
